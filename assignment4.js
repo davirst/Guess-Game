5dc51bfbfd39c1dc2dee7fc65d549858832d3541
davirst <67926435+davirst@users.noreply.github.com>
2020-07-06 23:20:31 -0600

    Add files via upload

---
+++ assignment4.js
@@ -0,0 +1,62 @@
+function setCookie(cbox,cvalue,exdays) 
+{
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cbox + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cbox) 
+{
+  var box = cbox + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(box) == 0) {
+      return c.substring(box.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() 
+{
+  var user=getCookie("userbox");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+     user = prompt("Please enter your box:","");
+
+     if (user != "" && user != null) {
+       setCookie("userbox", user, 30);
+       document.getElementById("third").innerHTML = "Hey " + user + ", what car is available for you today?" ;
+     }
+       show();
+  }
+}
+
+function show(){
+	var box = document.getElementById("box");
+	box.style.visibility = 'visible';
+	var side = document.getElementById("side");
+	side.style.visibility = 'visible';
+	var numCount = document.getElementById("numCount");
+	numCount.style.visibility = 'visible';
+}
+
+function showbox(){	
+	var image = document.getElementById("image");
+	image.style.visibility = 'visible';
+	$(image).fadeIn("40");
+}
+
+function checkcount() {
+  for (var i =0; i<2; i++);
+    console.count();
+}
+
+
---
+++ game.js
@@ -0,0 +1,13 @@
+function FerrariClick(){
+  alert("Ferrari is correct");
+}
+function LamborghiniClick() {
+  alert("NO Way! try again");
+}
+function DodgeClick() {
+  alert("NO Way! try again");
+}
+function MercedesClick() {
+  alert("NO Way! try again");
+}
+
