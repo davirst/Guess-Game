c0fd00c0971f6fc58fef15fce869eec500324488
davir <ddebrah@ymail.com>
2022-05-06 10:54:36 -0600

    Game JS

--- Pages/Home/HomeJS/game.js
+++ Pages/Home/HomeJS/game.js
@@ -1,12 +1,15 @@
 function FerrariClick(){
   alert("Ferrari is correct");
 }
+
 function LamborghiniClick() {
   alert("NO Way! try again");
 }
+
 function DodgeClick() {
   alert("NO Way! try again");
 }
+
 function MercedesClick() {
   alert("NO Way! try again");
 }
